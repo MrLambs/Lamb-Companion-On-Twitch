b802b5d03ba8df2cfd753b6751432a36d8e6febf
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-09-18 19:16:57 -0700

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const CHANNEL_NAMES = [ '#mrlamblambs', '#countedsheep' ];
+export const OAUTH_TOKEN = 'oauth:0lw1niydblaq2jkodr8p4zqwncvnqi';
+export const BOT_USERNAME = 'lamb_companion';
+
+export const BLOCKED_WORDS = [
+    'test delete'
+]
