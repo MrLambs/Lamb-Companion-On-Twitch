682b20a3d5b461c7d34ee454d85134212327353f
Matt Lambert <50882540+MrLambs@users.noreply.github.com>
2020-10-10 15:33:55 -0700

    💫 Updated logon.js
    > Incorporated switch/case for multiple user channels
    
    > Updated socials for user channels

--- src/events/client/logon.js
+++ src/events/client/logon.js
@@ -2,12 +2,14 @@ module.exports = async (client) => {
     console.log('[LOGS] Lamb_Companion is online!')
     setInterval(() => {
         let rpsMessage = "Try your hand at gambling with Rock, Paper, Scissors against me! (!rps [bet] [rock/paper/scissors])"
+        let twitterMessage;
+        let discordMessage;
         let randomMessageArr = [];
         ['countedsheep', 'enrix_tv'].forEach(ch => {
             switch (ch) {
                 case 'countedsheep':
-                    let twitterMessage = "Don't forget to follow me on Twitter for Stream announcements and the latest news. This is the best place to stay up to date with me. https://twitter.com/countedsheep1";
-                    let discordMessage = "Join my Discord - The Sheep Pen! You will gain access to stream announcements and can even play games with me! https://discord.gg/cNkt8rk";
+                    twitterMessage = "Don't forget to follow me on Twitter for Stream announcements and the latest news. This is the best place to stay up to date with me. https://twitter.com/CountedsheepTTV?s=09";
+                    discordMessage = "Join my Discord - The Sheep Pen! You will gain access to stream announcements and can even play games with me! https://discord.gg/cNkt8rk";
                     randomMessageArr = [];
                     randomMessageArr.push(twitterMessage, discordMessage, rpsMessage);
                     client.say('countedsheep', randomMessageArr[Math.floor(Math.random() * randomMessageArr.length)]);
@@ -18,6 +20,13 @@ module.exports = async (client) => {
                     client.say('enrix_tv', randomMessageArr[Math.floor(Math.random() * randomMessageArr.length)])
                     break;
                 case 'someguywithachair':
+                    break;
+                case '#prlmordial':
+                    twitterMessage = "Don't forget to follow me on Twitter for Stream announcements and the latest news. This is the best place to stay up to date with me. https://twitter.com/MrParadox115?s=09";
+                    discordMessage = "Join my Discord - Questionably Questionable! You will gain access to stream announcements and can even play games with me! https://discord.gg/pWzJEYJ";
+                    randomMessageArr = [];
+                    randomMessageArr.push(twitterMessage, discordMessage, rpsMessage);
+                    client.say('prlmordial', randomMessageArr[Math.floor(Math.random() * randomMessageArr.length)]);
                     break;
             }
         })
